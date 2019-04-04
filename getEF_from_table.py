e55f7dc5cf5cfe023d4e178056b8c691d1f8aba3
Kavishwar Wagholikar <waghsbox@gmail.com>
2019-04-04 18:56:15 -0400

    Update getEF_from_table.py
    Debug from Wyliena Guan

--- getEF_from_table.py
+++ getEF_from_table.py
@@ -7,7 +7,7 @@ def getEF_from_table(inTxt):
     
     #lv ejection fraction: 58 % (range: 54 - 73)
     pat="(LV EF|Ejection Fraction)[\d]{0,2} ((\d|\.)+) \
-         (%|Percent|percent)(.{0,10}) \ (Range: 50 - 75\)"
+         (%|Percent|percent)(.{0,10}) \(Range: 50 - 75\)"
     searchObj1 = (re.search( pat, txt, re.M|re.I))
     if searchObj1 and (len(searchObj1.groups())>=1):
         return (searchObj1.group(2).strip(),"pat1",searchObj1.group(0))
